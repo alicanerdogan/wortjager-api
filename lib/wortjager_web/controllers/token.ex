786ceda5b353a59cec6c8a328ab3b559f462d330
alicanerdogan <aerdogan07@gmail.com>
2017-08-25 00:53:33 +0200

    Added error codes for authorization/authentication

--- lib/wortjager_web/controllers/token.ex
+++ lib/wortjager_web/controllers/token.ex
@@ -3,11 +3,13 @@ defmodule WortjagerWeb.Token do
 
   def unauthenticated(conn, _params) do
     conn
+    |> put_status(:bad_request)
     |> render(WortjagerWeb.ErrorView, "error.json", error: "unauthenticated")
   end
 
   def unauthorized(conn, _params) do
     conn
+    |> put_status(:unauthorized)
     |> render(WortjagerWeb.ErrorView, "error.json", error: "unauthorized")
   end
 end
